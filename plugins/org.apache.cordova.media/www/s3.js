e2d4cfdbfe5869e4fce5a3914d7bf6ef7d294638
GiorgioNatili <g.natili@gnstudio.com>
2015-09-15 09:46:11 -0400

    Add the cordova devault files and plugins

---
+++ plugins/org.apache.cordova.media/www/s3.js
@@ -0,0 +1,11 @@
+cordova.define("org.apache.cordova.media.s3", function(require, exports, module) { /*global cordova, module*/
+
+module.exports = {
+
+  uploadFileWithSessionToken: function (sourceFile, regionName, bucketName, destinationFilePath, destinationFileNamePrefix, accessKey, secretKey, sessionToken, deleteFileAfterUpload, progressUpdateFunctionName, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "AWSS3", "cordovaUploadFile", [sourceFile, regionName, bucketName, destinationFilePath, destinationFileNamePrefix, accessKey, secretKey, sessionToken, deleteFileAfterUpload, progressUpdateFunctionName]);
+  }
+
+};
+
+});
